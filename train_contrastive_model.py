b43d3268fd3165781415ebd351c8fe54e7a26e2c
Lasze <Laszer2710@gmail.com>
2022-06-05 07:23:48 +0200

    update

--- train_contrastive_model.py
+++ train_contrastive_model.py
@@ -27,18 +27,24 @@ criterion = torch.nn.TripletMarginWithDistanceLoss(distance_function=loss)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-#model = torchvision.models.vit_b_16(pretrained=True)
-#model.heads.head = torch.nn.Identity()
+model = torchvision.models.vit_b_16(pretrained=True)
+model.heads.head = torch.nn.Identity()
+for param in model.parameters():
+    param.requires_grad = False
+for param in model.encoder.layers.encoder_layer_11.parameters():
+    param.requires_grad = True
 
 #model = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
 #model.classifier.fc = torch.nn.Identity()
 
+'''
 model = torchvision.models.resnext50_32x4d(pretrained=True)
 model.fc = torch.nn.Identity()
 for param in model.parameters():
     param.requires_grad = False
 for param in model.layer4[2].parameters():
     param.requires_grad = True
+'''
 
 model = model.to(device)
 
