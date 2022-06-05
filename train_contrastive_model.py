23021b93f82d557a20885d8e6377d41ecc51e144
Lasze <Laszer2710@gmail.com>
2022-06-05 05:05:49 +0200

    update

--- train_contrastive_model.py
+++ train_contrastive_model.py
@@ -188,8 +188,7 @@ def get_predictions(embeddings_ref, embeddings_val, labels_ref, distance):
   for emb_val in embeddings_val:
     distances = torch.Tensor([distance(emb_val, emb_ref) for emb_ref in embeddings_ref])
     vals, indices = torch.topk(distances, K, largest=False)
-    print(indices)
-    labels_ref = labels_ref[indices]
+    labels_ref = torch.index_select(labels_ref, 0, indices)
 
     pred_label = torch.mean(vals / sum(vals) * labels_ref)
     pred_label = torch.round(pred_label)
