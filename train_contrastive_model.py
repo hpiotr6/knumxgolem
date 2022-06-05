aff95047cbc1e2e9af2265beae2658a6374d4544
Lasze <Laszer2710@gmail.com>
2022-06-05 09:45:30 +0200

    update

--- train_contrastive_model.py
+++ train_contrastive_model.py
@@ -196,19 +196,18 @@ def matplotlib_imshow(img, one_channel=False):
 
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
-transform_Gauss = A.Compose(
-    [
-        A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+transform_Gauss = A.Compose([   
+        A.Resize(height=224, width=224),
         A.Rotate(p=0.5),
         A.Transpose(p=0.5),
         A.OpticalDistortion(p=0.5),
         A.Lambda(p=0.5),
         A.Blur(blur_limit=7, p=0.5),
-        # A.Perspective(p=1)
-        A.Normalize(),
-        ToTensorV2(),
-    ]
-)
+        A.GaussNoise(var_limit=(10.0, 50.0), mean=50, always_apply=False, p=0.5),
+        A.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=10, val_shift_limit=20, always_apply=False, p=0.5),
+        A.RandomFog(p=0.5),
+        A.RandomToneCurve(scale=0.1, always_apply=False, p=0.5),
+    ])
 
 ref_dataset = ReferenceDataset(
     img_dir=os.path.join(path, "cropped_ref"),
