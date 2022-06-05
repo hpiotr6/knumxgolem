8c84c8bd531e30f65ce52eec1935f290cfb4c6a9
Lasze <Laszer2710@gmail.com>
2022-06-05 13:03:44 +0200

    update preds

--- predictions.py
+++ predictions.py
@@ -213,9 +213,9 @@ if __name__ == '__main__':
         acc = evaluate_dataset(*eval_args, **eval_kwargs)
         print('Before training accuracy:', acc.item())
 
-        preds = predict_dataset(model, dataloader_ref, dataloader_val, loss, aug_ref=None, aug_times=0)
+        preds, _ = predict_dataset(model, dataloader_ref, dataloader_val, loss, aug_ref=None, aug_times=0)
 
-    print(dataloader_val.annotations)
+    print(val_dataset.annotations.shape)
     print(preds)
     import json
 
